0b7b962468686f70fee5cc5a343501f331b1652e
Siyris Om <hello@siyris.com>
2022-06-25 23:01:49 -0400

    updated interactive_canvas.dart

--- test/icanvas_test.dart
+++ test/icanvas_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:interactive_canvas/icanvas.dart';
+import 'package:interactive_canvas/interactive_canvas.dart';
 
 main() => runApp(MyApp());
 
